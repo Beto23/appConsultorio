b62fa0db56450bd50bc72f20179d9785970a23ef
Beto23 <josee-45@hotmail.com>
2016-03-06 00:46:19 -0600

    add header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var angular = require('angular');
 require ('angular-ui-router');
 window.$ = window.jQuery = require('jquery');
+require('../node_modules/bootstrap/dist/js/bootstrap.js')
 
 var app = angular.module('appConsultorio', ['ui.router', 'appConsultorio.home']);
 require('./app/home/home.module')();
