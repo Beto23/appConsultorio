225c2def18477a23f8e26b0cd2e64acc86365c10
Beto23 <josee-45@hotmail.com>
2016-03-13 16:55:21 -0600

    se agrego btn desplegable

--- src/app/main/secretaria/secretaria.module.js
+++ src/app/main/secretaria/secretaria.module.js
@@ -1,4 +1,6 @@
 module.exports = function (){
   var secretaria = angular.module('appConsultorio.main.secretaria',[])
-  require('./consultas/consultas.controller')(secretaria)
+  require('./consultas/consultas.controller')(secretaria);
+  require('./secretaria.controller')(secretaria);
+
 }
