41a97343e4df66264aee73c6537b8d7dcc2f9657
Beto23 <josee-45@hotmail.com>
2016-03-15 00:40:32 -0600

    se agrego funcionalidad a tabs expediente

--- src/helpers/helpers.factory.js
+++ src/helpers/helpers.factory.js
@@ -4,7 +4,7 @@ module.exports = function(ngModule){
   function HelpersFactory (){
     var Helper = {
       twoTabs: function (){
-        console.log('factory');
+        console.log('factory 4');
         var postConsulta = $('#postCnst');
         var getConsulta = $('#getCnst');
 
@@ -24,6 +24,57 @@ module.exports = function(ngModule){
             console.log('get desactivado');
           }
         })
+      },
+      forTabs: function (){
+        console.log('444');
+        var tab1 = $('#tab1');
+        var tab2 = $('#tab2');
+        var tab3 = $('#tab3');
+        var tab4 = $('#tab4');
+
+        tab1.click(function(){
+          $(this).addClass('active');
+          if (tab2.hasClass('active')) {
+            tab2.removeClass('active')
+          }else if (tab3.hasClass('active')) {
+            tab3.removeClass('active')
+          }else if (tab4.hasClass('active')) {
+            tab4.removeClass('active')
+          }
+        });
+
+        tab2.click(function(){
+          $(this).addClass('active');
+          if (tab1.hasClass('active')) {
+            tab1.removeClass('active')
+          }else if (tab3.hasClass('active')) {
+            tab3.removeClass('active')
+          }else if (tab4.hasClass('active')) {
+            tab4.removeClass('active')
+          }
+        });
+
+        tab3.click(function(){
+          $(this).addClass('active');
+          if (tab2.hasClass('active')) {
+            tab2.removeClass('active')
+          }else if (tab1.hasClass('active')) {
+            tab1.removeClass('active')
+          }else if (tab4.hasClass('active')) {
+            tab4.removeClass('active')
+          }
+        });
+
+        tab4.click(function(){
+          $(this).addClass('active');
+          if (tab2.hasClass('active')) {
+            tab2.removeClass('active')
+          }else if (tab3.hasClass('active')) {
+            tab3.removeClass('active')
+          }else if (tab1.hasClass('active')) {
+            tab1.removeClass('active')
+          }
+        });
       }
     }
     return Helper;
