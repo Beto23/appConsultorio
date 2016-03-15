da51b4805105f717e16083e8b28d2420a24ca276
Beto23 <josee-45@hotmail.com>
2016-03-14 19:50:17 -0600

    se injecto HelpersFactory con twoTabs()

--- src/app/main/secretaria/consultas/consultas.controller.js
+++ src/app/main/secretaria/consultas/consultas.controller.js
@@ -1,26 +1,8 @@
 module.exports = function (ngModule){
   ngModule.controller('ConsultasCtrl',ConsultasCtrl)
   /*@ngInject*/
-  function ConsultasCtrl (){
+  function ConsultasCtrl (HelpersFactory){
     console.log('controller consultas')
-    var postConsulta = $('#postCnst');
-    var getConsulta = $('#getCnst');
-
-    getConsulta.click(function(){
-      $(this).addClass('active');
-      if (postConsulta.hasClass("active")) {
-        postConsulta.removeClass('active')
-      }else {
-        console.log('post desactivado');
-      }
-    })
-    postConsulta.click(function(){
-      $(this).addClass('active');
-      if (getConsulta.hasClass("active")) {
-        getConsulta.removeClass('active')
-      }else {
-        console.log('get desactivado');
-      }
-    })
+    HelpersFactory.twoTabs();
   }
 }
