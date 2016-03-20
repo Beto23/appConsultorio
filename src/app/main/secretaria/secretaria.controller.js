1a1155b7027e0e471ffd223a08e4659fd282cd40
Beto23 <josee-45@hotmail.com>
2016-03-20 16:53:12 -0600

    se agrego menu-item 5

--- src/app/main/secretaria/secretaria.controller.js
+++ src/app/main/secretaria/secretaria.controller.js
@@ -17,6 +17,7 @@ module.exports = function (ngModule){
     var menu_item2 = $('#menu-item2');
     var menu_item3 = $('#menu-item3');
     var menu_item4 = $('#menu-item4');
+    var menu_item5 = $('#menu-item5');
 
     menu_item1.click(function(event){
       $(this).addClass('active2');
@@ -26,6 +27,8 @@ module.exports = function (ngModule){
         menu_item3.removeClass('active2');
       }else if (menu_item4.hasClass('active2')) {
         menu_item4.removeClass('active2');
+      }else if (menu_item5.hasClass('active2')) {
+        menu_item5.removeClass('active2');
       }
     });
     menu_item2.click(function(event){
@@ -36,6 +39,8 @@ module.exports = function (ngModule){
       menu_item3.removeClass('active2');
       }else if (menu_item4.hasClass('active2')) {
         menu_item4.removeClass('active2');
+      }else if (menu_item5.hasClass('active2')) {
+        menu_item5.removeClass('active2');
       }
     });
     menu_item3.click(function(event){
@@ -46,6 +51,8 @@ module.exports = function (ngModule){
         menu_item1.removeClass('active2');
       }else if (menu_item4.hasClass('active2')) {
         menu_item4.removeClass('active2');
+      }else if (menu_item5.hasClass('active2')) {
+        menu_item5.removeClass('active2');
       }
     });
     menu_item4.click(function(event){
@@ -54,6 +61,20 @@ module.exports = function (ngModule){
         menu_item2.removeClass('active2');
       }else if (menu_item3.hasClass('active2')) {
         menu_item3.removeClass('active2');
+      }else if (menu_item1.hasClass('active2')) {
+        menu_item1.removeClass('active2');
+      }else if (menu_item5.hasClass('active2')) {
+        menu_item5.removeClass('active2');
+      }
+    });
+    menu_item5.click(function(event){
+      $(this).addClass('active2');
+      if (menu_item2.hasClass('active2')) {
+        menu_item2.removeClass('active2');
+      }else if (menu_item3.hasClass('active2')) {
+        menu_item3.removeClass('active2');
+      }else if (menu_item4.hasClass('active2')) {
+        menu_item4.removeClass('active2');
       }else if (menu_item1.hasClass('active2')) {
         menu_item1.removeClass('active2');
       }
