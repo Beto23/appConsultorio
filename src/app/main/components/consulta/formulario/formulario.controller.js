fdf3abb3a164b20826e139198167495506bc9590
Beto23 <josee-45@hotmail.com>
2016-03-23 01:40:49 -0600

    se agrego datapicker a formulario consultas

--- src/app/main/components/consulta/formulario/formulario.controller.js
+++ src/app/main/components/consulta/formulario/formulario.controller.js
@@ -2,7 +2,39 @@ module.exports = function(ngModule){
   ngModule.controller('ConsultaFormularioCtrl',ConsultaFormularioCtrl)
 
   /*@ngInject*/
-  function ConsultaFormularioCtrl (){
+  function ConsultaFormularioCtrl ($scope){
+    /*datapicker*/
+    $scope.today = function() {
+      $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function() {
+      $scope.dt = null;
+    };
+    $scope.dateOptions = {
+      dateDisabled: disabled,
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22),
+      minDate: new Date(),
+      startingDay: 1
+    };
+    // Disable weekend selection
+    function disabled(data) {
+      var date = data.date,
+        mode = data.mode;
+      return mode === 'day' && (date.getDay() === -1 || date.getDay() === 8);
+    }
+    $scope.open2 = function() {
+      $scope.popup2.opened = true;
+    };
+    $scope.setDate = function(year, month, day) {
+      $scope.dt = new Date(year, month, day);
+    };
+    $scope.popup2 = {
+      opened: false
+    };
+    /*datapicker*/
     var vm = this;
     vm.optionFormulario = {};
     vm.functionSelected = functionSelected;
