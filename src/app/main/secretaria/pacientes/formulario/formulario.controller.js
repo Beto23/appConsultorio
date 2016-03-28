fe39447074057dd2489f2c85c7e89f243da73c3f
Beto23 <josee-45@hotmail.com>
2016-03-28 13:04:27 -0600

    se agrego functionSelected

--- src/app/main/secretaria/pacientes/formulario/formulario.controller.js
+++ src/app/main/secretaria/pacientes/formulario/formulario.controller.js
@@ -3,6 +3,34 @@ module.exports = function(ngModule){
 
   /*@ngInject*/
   function PacienteFormularioCtrl (){
-    console.log('paciente fomr');
+    var vm = this;
+    vm.optionFormulario = {};
+    vm.functionSelected = functionSelected;
+
+
+    if(vm.paciente){
+      vm.optionFormulario.buttonSubmit = 'Editar'
+      vm.optionFormulario.encabezadoTittle = 'Editar Paciente'
+    } else {
+      vm.optionFormulario.buttonSubmit = 'Agregar'
+      vm.optionFormulario.encabezadoTittle = 'Agregar Paciente'
+    }
+
+
+    function functionSelected(){
+      if(vm.paciente){
+        editarPaciente();
+      } else {
+        agregarPaciente();
+      }
+    }
+
+    function editarPaciente(){
+      console.log('editarPaciente');
+    }
+
+    function agregarPaciente(){
+      console.log('agregarPaciente');
+    }
   }
 }
