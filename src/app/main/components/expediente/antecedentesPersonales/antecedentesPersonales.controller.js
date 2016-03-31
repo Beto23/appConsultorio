36fec23d9ed54616b672a3184384e77a09285976
Beto23 <josee-45@hotmail.com>
2016-03-31 16:38:21 -0600

    se agrego funcionalidad de btn-antecedentes

--- src/app/main/components/expediente/antecedentesPersonales/antecedentesPersonales.controller.js
+++ src/app/main/components/expediente/antecedentesPersonales/antecedentesPersonales.controller.js
@@ -4,5 +4,35 @@ module.exports = function (ngModule){
   /*@ngInject*/
   function AntPersoCtrl (){
     console.log('antecedentes-personales');
+
+    var menu_antecedentes1 = $('#menu_antecedentes1');
+    var menu_antecedentes2 = $('#menu_antecedentes2');
+    var menu_antecedentes3 = $('#menu_antecedentes3');
+
+    menu_antecedentes1.click(function(){
+       $(this).addClass('menu-active')
+       if (menu_antecedentes2.hasClass('menu-active')) {
+           menu_antecedentes2.removeClass('menu-active')
+       }else if (menu_antecedentes3.hasClass('menu-active')) {
+         menu_antecedentes3.removeClass('menu-active')
+       }
+     })
+
+    menu_antecedentes2.click(function(){
+       $(this).addClass('menu-active')
+       if (menu_antecedentes1.hasClass('menu-active')) {
+           menu_antecedentes1.removeClass('menu-active')
+       }else if (menu_antecedentes3.hasClass('menu-active')) {
+         menu_antecedentes3.removeClass('menu-active')
+       }
+     })
+    menu_antecedentes3.click(function(){
+       $(this).addClass('menu-active')
+       if (menu_antecedentes1.hasClass('menu-active')) {
+           menu_antecedentes1.removeClass('menu-active')
+       }else if (menu_antecedentes2.hasClass('menu-active')) {
+         menu_antecedentes2.removeClass('menu-active')
+       }
+     })
   }
 }
