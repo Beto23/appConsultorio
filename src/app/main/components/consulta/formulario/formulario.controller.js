aa5e067e73ea34dc89ae39e93cb06f17d10388fb
Edgar Zavala <ezavile@gmail.com>
2016-03-31 00:53:09 -0600

    fixing datepicker with dates

--- src/app/main/components/consulta/formulario/formulario.controller.js
+++ src/app/main/components/consulta/formulario/formulario.controller.js
@@ -4,40 +4,13 @@ module.exports = function(ngModule){
   /*@ngInject*/
   function ConsultaFormularioCtrl ($scope){
     /*datapicker*/
-    $scope.today = function() {
-      $scope.dt = new Date();
-    };
-    $scope.today();
-
-    $scope.clear = function() {
-      $scope.dt = null;
-    };
-    $scope.dateOptions = {
-      dateDisabled: disabled,
-      formatYear: 'yy',
-      maxDate: new Date(2020, 5, 22),
-      minDate: new Date(),
-      startingDay: 1
-    };
-    // Disable weekend selection
-    function disabled(data) {
-      var date = data.date,
-        mode = data.mode;
-      return mode === 'day' && (date.getDay() === -1 || date.getDay() === 8);
-    }
-    $scope.open2 = function() {
-      $scope.popup2.opened = true;
-    };
-    $scope.setDate = function(year, month, day) {
-      $scope.dt = new Date(year, month, day);
-    };
-    $scope.popup2 = {
-      opened: false
-    };
-    /*datapicker*/
     var vm = this;
+    vm.datePickerOn = false;
     vm.optionFormulario = {};
     vm.functionSelected = functionSelected;
+
+    vm.dateInDatePicker = moment(vm.paciente.fecha).toDate();
+
 
 
     if(vm.paciente){
