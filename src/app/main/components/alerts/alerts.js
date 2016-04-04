6a975ec92623c4904c2308767994af876f6c2912
Beto23 <josee-45@hotmail.com>
2016-04-04 18:02:13 -0500

    se agrego diseño de alert bug

--- src/app/main/components/alerts/alerts.js
+++ src/app/main/components/alerts/alerts.js
@@ -3,4 +3,6 @@ module.exports = function(ngModule) {
   require('./success/success.directive')(ngModule);
   require('./warning/warning.controller')(ngModule);
   require('./warning/warning.directive')(ngModule);
+  require('./bug/bug.controller')(ngModule);
+  require('./bug/bug.directive')(ngModule);
 };
